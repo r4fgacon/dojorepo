199274f962fa9c6c72b6d246c8cc89cc0dc13ce3
r4fgacon <r4fgacon@gmail.com>
2018-10-09 09:45:03 +0200

    shout

--- gittie.py
+++ gittie.py
@@ -1,9 +1,16 @@
 def introduce():
     print("Hello , I'm Gittie!")
 
+
 def add():
     a = input("First number:")
     b = input("Second number:")
     print("Answer: ", a + b)
+
+
 def joke():
     print("Knock, knock. Who's there!. Yo mama!")
+
+
+def shout():
+    print("FUCK THE POLICE COMING STRAIGHT FROM UNDERGROUND")
